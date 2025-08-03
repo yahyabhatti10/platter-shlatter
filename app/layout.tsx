3d8c6356c1be91601b8d23a0f1682782d7bbfbef
Muhammad Yahya <yahyabhatti019@gmail.com>
2025-08-03 20:47:57 +0500

    Added analytics in main app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
@@ -46,6 +47,7 @@ export default function RootLayout({
         <main className="min-h-screen">{children}</main>
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
